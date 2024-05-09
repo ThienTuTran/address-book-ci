ed8bcfd1b2aa52779abbbdb05fee219638c74c8f
Thien <tuthien.tran@connect.qut.edu.au>
2024-05-09 16:56:06 +1000

    Update project structure to use Maven's Standard Directory Layout

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.addressbook {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+//    requires org.junit.jupiter.api;
 
 
     opens com.example.addressbook to javafx.fxml;
